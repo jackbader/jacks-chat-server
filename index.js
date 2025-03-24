7cd4e36cc88d3deca7e375b646545e2ae5c2650f
Jack Bader <jackbader@Jacks-MacBook-Pro.local>
2025-03-23 23:41:45 -0400

    Prepare for deployment

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ dotenv.config();
 
 // Create Express app
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true
+}));
 app.use(express.json());
 
 // Create HTTP server
